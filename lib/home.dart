19830179692f5d23af92a1cf6fc63126510c5ada
Sinan Alagöz <sinanalagoz74@gmail.com>
2023-01-28 16:24:42 +0300

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,159 @@
+
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  late double screenHeight, screenWidth;
+  bool isOpenMenu = false;
+  // late AnimationController animationController;
+  // late Animation scaleAnimation;
+  Duration duration = const Duration(milliseconds: 500);
+
+  // @override
+  // void initState() {
+   
+  //   super.initState();
+  //   animationController = AnimationController(vsync: this, duration: duration);
+  //   // scaleAnimation = Tween(begin: 1, end: .5).animate(animationController);
+  // }
+
+  // @override
+  // void dispose() {
+  //   animationController.dispose();
+    
+  //   super.dispose();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('Material App Bar'),
+      // ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            const Menu(),
+            dashBoard(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  AnimatedPositioned dashBoard() {
+    return AnimatedPositioned(
+      top: isOpenMenu ? 0.02 * screenHeight : 0,
+      bottom: isOpenMenu ? .02 * screenHeight : 0,
+      left: isOpenMenu ? .4 * screenWidth : 0,
+      right: isOpenMenu ? .02 * screenWidth : 0,
+      duration: duration,
+      child: Material(
+        borderRadius:
+            isOpenMenu ? BorderRadius.circular(20) : BorderRadius.circular(0),
+        elevation: 20,
+        child: Container(
+          padding: const EdgeInsets.only(left: 5, right: 5, top: 5, bottom: 5),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 8.0, right: 8, top: 10, bottom: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            isOpenMenu = !isOpenMenu;
+                           
+                          });
+                        },
+                        child: const Icon(Icons.menu)),
+                    const Text('My Cards'),
+                    const Icon(Icons.add_circle_outline),
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
+                height: 300,
+                child: PageView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Container(
+                      color: Colors.red,
+                      width: 100,
+                      height: 100,
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                    ),
+                    Container(
+                      color: Colors.blue,
+                      width: 100,
+                      height: 100,
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                    ),
+                    Container(
+                      color: Colors.green,
+                      width: 100,
+                      height: 100,
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Menu extends StatelessWidget {
+  const Menu({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text('Dashboard'),
+            SizedBox(
+              height: 10,
+            ),
+            Text('Messages'),
+            SizedBox(
+              height: 10,
+            ),
+            Text('Utility Bills'),
+            SizedBox(
+              height: 10,
+            ),
+            Text('Fund Transfer'),
+            SizedBox(
+              height: 10,
+            ),
+            Text('Branches'),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'home.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent,));
+   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [
+  SystemUiOverlay.bottom
+]);
+  // SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
+
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((value) {
+    runApp(const MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      title: 'Material App',
+      home: const Home(),
+    );
+  }
+}
+
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+  
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+
+  
+  
+  
+
+  
+  
+  
+  
+
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+
+//   void _incrementCounter() {
+//     setState(() {
+      
+      
+      
+      
+      
+//       _counter++;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+    
+    
+    
+    
+    
+    
+//     return Scaffold(
+//       appBar: AppBar(
+        
+        
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+        
+        
+//         child: Column(
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headlineMedium,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ), 
+//     );
+//   }
+// }
